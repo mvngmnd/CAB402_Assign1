01bde37a4693bfd49c175f7716ab72d2fc5df05e
Marcus van Egmond <mvngmnd@gmail.com>
2019-03-25 13:59:06 +1000

    Add basic pure functionality. Choose datatypes/implement minimal methods.

--- TicTacToe/FSharpTicTacToeModels/TicTacToePure.fs
+++ TicTacToe/FSharpTicTacToeModels/TicTacToePure.fs
@@ -7,48 +7,72 @@ namespace QUT
 
         // type to represent a single move specified using (row, column) coordinates of the selected square
         type Move = 
-            { something: int (* TODO implement type *) }
+            {
+                Row: int;
+                Column: int
+            }
             interface ITicTacToeMove with
-                member this.Row with get() = raise (System.NotImplementedException("getRow"))
-                member this.Col with get() = raise (System.NotImplementedException("getCol"))
+                member this.Row with get() = this.Row
+                member this.Col with get() = this.Column
 
         // type to represent the current state of the game, including the size of the game (NxN), who's turn it is and the pieces on the board
         type GameState = 
-            { something: int (* TODO implement type *) }
+            {
+                GameTurn: Player;
+                GameSize: int;
+                GameBoard: Map<int*int, Player>
+            }
             interface ITicTacToeGame<Player> with
-                member this.Turn with get()    = raise (System.NotImplementedException("getTurn"))
-                member this.Size with get()    = raise (System.NotImplementedException("getSize"))
-                member this.getPiece(row, col) = raise (System.NotImplementedException("getPiece"))
+                member this.Turn with get()    = this.GameTurn
+                member this.Size with get()    = this.GameSize
+                member this.getPiece(row, col) = 
+                    if this.GameBoard.ContainsKey(row, col) then
+                        match this.GameBoard.Item(row,col) with
+                        | Player.Cross -> "X"
+                        | Player.Nought -> "O"
+                    else
+                        ""
 
 
-        let CreateMove row col = raise (System.NotImplementedException("CreateMove"))
+        let CreateMove row col = 
+            {Row = row; Column = col}
 
-        let ApplyMove (oldState:GameState) (move: Move) = raise (System.NotImplementedException("CreateMove"))
-
-        // Returns a sequence containing all of the lines on the board: Horizontal, Vertical and Diagonal
-        // The number of lines returned should always be (size*2+2)
-        // the number of squares in each line (represented by (row,column) coordinates) should always be equal to size
-        // For example, if the input size = 2, then the output would be: 
-        //     seq [seq[(0,0);(0,1)];seq[(1,0);(1,1)];seq[(0,0);(1,0)];seq[(0,1);(1,1)];seq[(0,0);(1,1)];seq[(0,1);(1,0)]]
-        // The order of the lines and the order of the squares within each line does not matter
-        let Lines (size:int) : seq<seq<int*int>> = raise (System.NotImplementedException("Lines"))
+        let ApplyMove (oldState:GameState) (move: Move) = 
+            let newPlayer = 
+                match oldState.GameTurn with
+                | Player.Cross -> Player.Nought
+                | Player.Nought -> Player.Cross
+            {GameSize = oldState.GameSize; GameBoard = oldState.GameBoard.Add((move.Column, move.Row), oldState.GameTurn); GameTurn = newPlayer }
+                
+        
+        let Lines (size:int) : seq<seq<int*int>> = 
+            let Rows : seq<seq<int*int>> = 
+                seq {for x = 0 to size-1 do
+                        yield seq{for y=0 to size-1 do yield (x,y)}
+                }
+            let Columns : seq<seq<int*int>> = 
+                seq {for y = 0 to size-1 do
+                        yield seq{for x=0 to size-1 do yield (x,y)}
+                }
+            let Diagonals =
+                let left = seq{yield seq {for x = 0 to size-1 do yield (x,x)}}
+                let right = seq{yield seq {for x = 0 to size-1 do yield (x, size-1 - (x%size))}}
+                Seq.append left right
+            Seq.append (Seq.append Rows Columns) Diagonals
 
         // Checks a single line (specified as a sequence of (row,column) coordinates) to determine if one of the players
         // has won by filling all of those squares, or a Draw if the line contains at least one Nought and one Cross
-        let CheckLine (game:GameState) (line:seq<int*int>) : TicTacToeOutcome<Player> = raise (System.NotImplementedException("CheckLine"))
+        let CheckLine (game:GameState) (line:seq<int*int>) : TicTacToeOutcome<Player> = 
+            Undecided 
 
-        let GameOutcome game = raise (System.NotImplementedException("GameOutcome"))
+        let GameOutcome game = Undecided
 
-        let GameStart (firstPlayer:Player) size = raise (System.NotImplementedException("GameStart"))
+        let GameStart (firstPlayer:Player) size = 
+            {GameTurn = firstPlayer; GameSize = size; GameBoard = Map.empty<int*int, Player>}
 
         let MiniMax game = raise (System.NotImplementedException("MiniMax"))
 
         let MiniMaxWithPruning game = raise (System.NotImplementedException("MiniMaxWithPruning"))
-
-        // plus other helper functions ...
-
-
-
 
         [<AbstractClass>]
         type Model() =
@@ -66,7 +90,6 @@ namespace QUT
             inherit Model()
             override this.ToString()         = "Pure F# with basic MiniMax";
             override this.FindBestMove(game) = raise (System.NotImplementedException("FindBestMove"))
-
 
         type WithAlphaBetaPruning() =
             inherit Model()
